5f381b9bb5008a7f39e968979c112fc4465f2bf1
wang.yl <spd0010@outlook.jp>
2021-08-05 16:49:59 +0900

    seller address cascader add

--- b2b2c-web/src/main/java/com/slodon/b2b2c/model/system/RegionDistrictModel.java
+++ b2b2c-web/src/main/java/com/slodon/b2b2c/model/system/RegionDistrictModel.java
@@ -6,11 +6,16 @@ import com.slodon.b2b2c.dao.read.system.RegionDistrictReadMapper;
 import com.slodon.b2b2c.dao.write.system.RegionDistrictWriteMapper;
 import com.slodon.b2b2c.system.example.RegionDistrictExample;
 import com.slodon.b2b2c.system.pojo.RegionDistrict;
+import com.slodon.b2b2c.vo.system.RegionVO;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -99,4 +104,33 @@ public class RegionDistrictModel {
         }
         return regionDistrictList;
     }
+
+    /**
+     * 根据条件获取区信息表
+     *
+     * @return
+     */
+    public List<RegionVO> getDistrictList(String remarkCode, String cityCode) {
+        List<RegionVO> list = new ArrayList<>();
+        RegionDistrictExample example = new RegionDistrictExample();
+        example.setRemarkCode(remarkCode);
+        if (!StringUtils.isEmpty(cityCode)) {
+            example.setCityCode(cityCode);
+        }
+        example.setOrderBy("native_code asc");
+        List<RegionDistrict> districtList = regionDistrictReadMapper.listByExample(example);
+        if (!CollectionUtils.isEmpty(districtList)) {
+            for (RegionDistrict district : districtList) {
+                RegionVO vo = new RegionVO();
+                vo.setParentCode(district.getCityCode());
+                vo.setRegionCode(district.getRemarkCode());
+                vo.setRegionName(district.getDistrictName());
+                vo.setRegionLevel(district.getRegionLevel());
+                vo.setChildren(new ArrayList<>());
+                list.add(vo);
+            }
+        }
+        return list;
+    }
+
 }
